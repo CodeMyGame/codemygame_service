d490425837ca906e83e16f3886b0288bf80e5a9d
codemygame <kapil.nituk@gmail.com>
2018-10-17 19:05:55 +0530

    first commit

---
+++ src/main/java/com/codemygame/CodemygameApplication.java
@@ -0,0 +1,12 @@
+package com.codemygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemygameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodemygameApplication.class, args);
+	}
+}
